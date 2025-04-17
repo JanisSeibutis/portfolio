b4e597957b3098fd65a770ec65445f01492178cd
JanisSeibutis <janis.seibutis@gmail.com>
2025-04-17 10:07:22 +0200

    feat: add scholl icon and link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,6 +17,17 @@ const Hero: React.FC = () => {
         </div>
         <p>Currently i am a student at Medieinstitutet in Stockholm, Sweden</p>
         <a 
+          href="https://medieinstitutet.se/utbildningar/front-end-developer/" 
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img 
+            src="/portfolio/images/medieinstitutet.png" 
+            alt="Medieinstitutet Logo" 
+            className="medieinstitutet-logo"
+          />
+        </a>
+        <a 
           href="/portfolio/Janis_Seibutis_CV.pdf" 
           className="cv-button"
           target="_blank"
